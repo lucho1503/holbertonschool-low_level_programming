716866751ab5423e73a1be2522629c3cfbd6ee25
lucho1503 <luis.2019.corpha@gmail.com>
2019-06-19 20:41:46 +0000

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /* hello world
- *programming
+ * programming
  */
 
 int main(void)	 
 {
-  puts('"Programming is like building a multilingual puzzle');
+  puts("Programming is like building a multilingual puzzle");
   return(0);
 }
