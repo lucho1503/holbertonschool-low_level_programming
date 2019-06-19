5ff4fb20a4e3217e2620a4bb81d26cf59ad343f2
lucho1503 <luis.2019.corpha@gmail.com>
2019-06-19 22:30:17 +0000

    sizeof

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("Size of int data type : %d\n", sizeof(int));
+printf("Size of char data type : %d\n", sizeof(char));
+printf("Size of float data type : %d\n", sizeof(float));
+printf("Size of double data type : %d\n", sizeof(double));
+return (0);
+}
