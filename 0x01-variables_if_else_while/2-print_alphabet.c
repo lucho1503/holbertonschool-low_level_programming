844d0dcbb38d33b590b78e6a5db417233ad994ff
lucho1503 <luis.2019.corpha@gmail.com>
2019-06-20 23:33:09 +0000

    print_alphabet_lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ **/
+int main(void)
+{
+char b;
+for (b = 'a'; b <= 'z'; b++)
+putchar(b);
+putchar('\n');
+return (0);
+}
