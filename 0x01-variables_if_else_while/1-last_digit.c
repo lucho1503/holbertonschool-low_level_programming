6ae34b437b4d37c3876435c5bba02047fb2be828
lucho1503 <luis.2019.corpha@gmail.com>
2019-06-20 21:19:40 +0000

    conditional and booleano

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ **/
+
+int main(void)
+{
+int n, last;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+last = n % 10;
+if (n > 5)
+{
+printf("last digit of %d is %d and is greather than 5 \n",n ,last);
+}
+else if (n < 6 && n != 0)
+{
+printf("last digit of %d is %d and is less than 6 and not 0\n",n ,last);
+}
+else if (n == 0)
+{
+printf("last digit of %d is %d and is 0\n",n ,last);
+}
+return (0);
+}
