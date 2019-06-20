253db725f4bd7d632ac67312346f60607da20f66
lucho1503 <luis.2019.corpha@gmail.com>
2019-06-20 21:52:31 +0000

    booleanos_conditiolnal

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -24,7 +24,7 @@ printf("last digit of %d is %d and is less than 6 and not 0\n", n, last);
 }
 else if (n == 0)
 {
-printf("last digit of %d is %d and is 0\n", n, last);
+printf("last digit of %d is 0 and is 0\n", n);
 }
 return (0);
 }
