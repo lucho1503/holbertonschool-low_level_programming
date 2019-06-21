bdf2ed0f2844d2c938cd1769b679676600ffb939
lucho1503 <luis.2019.corpha@gmail.com>
2019-06-21 21:43:25 +0000

    print 0-9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: All single digit numbers of base 10 starting from 0
+ **/
+int main(void)
+{
+int z;
+for (z = '0'; z <= '9'; z++)
+{
+putchar(z);
+if (z != '9')
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
