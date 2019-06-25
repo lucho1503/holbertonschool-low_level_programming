b8854744a32ea6b588965c254757383ba5df4118
lucho1503 <luis.2019.corpha@gmail.com>
2019-06-25 22:57:47 +0000

    number_absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * _abs - computer the number absolute.
+ *
+ * @r: number absolute in negatives or positives.
+ *
+ * Return: 1 if is positive o negative  or 0 if not is number absolute.
+ */
+int _abs(int r)
+{
+if (r > 0)
+{
+return (r);
+}
+else
+{
+return (-r);
+}
+}
