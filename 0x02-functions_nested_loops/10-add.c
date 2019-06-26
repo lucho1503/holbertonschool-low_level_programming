64b02413f2b831dd4b15756830df392226aebce9
lucho1503 <luis.2019.corpha@gmail.com>
2019-06-26 01:33:13 +0000

    adds_ numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * print_last_digit - last digit.
+ *
+ * @i: find the last digit.
+ *
+ * Return: the last digit.
+ */
+int add(int x, int y)
+{
+	int z = x + y;
+	return (z);
+}
