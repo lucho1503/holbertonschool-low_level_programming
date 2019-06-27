ac524a3c40df433da35e454ffb5acf7fcf9f8142
lucho1503 <luis.2019.corpha@gmail.com>
2019-06-27 00:46:09 +0000

    fibbonacci_adds

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,29 +1,25 @@
 #include <stdio.h>
 /**
- * times_table - checks if negative or positive.
+ * main - fibonnacci number adds.
  *
- * @x, y, z: print tables 0 - 9.
+ *
  *
  * Return: alwasys 0.
  */
 
 int main(void)
 {
-	long int y, a;
-	int z = 1;
-	int x = 2;
-	printf("%d, %d, ", z, x);
-	for (a = 1; a <= 50; a++)
-		{
-			y = x + z;
-			z = x;
-			x = y;
-			printf("%ld", y);
-			putchar(',');
-			putchar(' ');
-			
-			
-		}
-	putchar('\n');
-	return (0);
+long long int y, a;
+int z = 1;
+int x = 2;
+printf("%d, %d, ", z, x);
+for (a = 1; a <= 50; a++)
+{
+y = x + z;
+z = x;
+x = y;
+printf("%lld, ", y);
+}
+putchar('\n');
+return (0);
 }
