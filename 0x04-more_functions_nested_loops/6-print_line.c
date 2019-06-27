a12152dcf93601149e0d9a12d01d12a61a229812
lucho1503 <luis.2019.corpha@gmail.com>
2019-06-27 21:11:17 +0000

    print lines_

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,22 +1,20 @@
 #include "holberton.h"
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * print_line - print lines.
+ * @n: variable that print n lines.
+ * Return: lines.
  **/
 
 void print_line(int n)
 {
-	int z;
-	
-	if (n > 0)
-		{
-	for (z = 0; z < n; z++)
-		{
-			_putchar(95);
-		}
-		}
-	_putchar('\n');	
-		
+int z;
+if (n > 0)
+{
+for (z = 0; z < n; z++)
+{
+_putchar(95);
+}
+}
+_putchar('\n');
 }
