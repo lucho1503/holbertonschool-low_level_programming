1aaba364200c7a3c3d950bc00e3c3c40460d87c3
lucho1503 <lmmc.1503@gmail.com>
2019-06-28 04:47:36 +0000

    print_triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -3,34 +3,32 @@
 /**
  * print_triangle - print to 1 to 1024 adds multiples 3 or 5 .
  *
- *
+ * @size: contents the prints numbers of #.
  *
  * Return: the variable size.
  */
 
 void print_triangle(int size)
 {
-	int x, y;
-	if (size > 0)
-		{
-	for (x = 1; x <= size ; x++)
-		{
-			for (y = 0; y < size; y++)
-			{
-				if (y < size - x)
-				{
-					 	
-					_putchar(' ');
-				}
-				else
-					_putchar('#');
-			}
-			_putchar('\n');
-		}
-	
-		}
-	else
-		{
-			_putchar('\n');
-		}
+int x, y;
+if (size > 0)
+{
+for (x = 1; x <= size ; x++)
+{
+for (y = 0; y < size; y++)
+{
+if (y < size - x)
+{
+_putchar(' ');
+}
+else
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+else
+{
+_putchar('\n');
+}
 }
