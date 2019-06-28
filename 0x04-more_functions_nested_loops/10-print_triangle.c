fce647b9cdc4f774c2bd33483508b1496374a1b1
lucho1503 <luis.2019.corpha@gmail.com>
2019-06-28 03:13:15 +0000

    10-print_triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+
+/**
+ * print_triangle - print to 1 to 1024 adds multiples 3 or 5 .
+ *
+ *
+ *
+ * Return: the variable size.
+ */
+
+void print_triangle(int size)
+{
+	int x, y;
+	if (size > 0)
+		{
+	for (x = 1; x <= size ; x++)
+		{
+			for (y = 0; y < size; y++)
+			{
+				if (y < size - x)
+				{
+					 	
+					_putchar(' ');
+				}
+				else
+					_putchar('#');
+			}
+			_putchar('\n');
+		}
+	
+		}
+	else
+		{
+			_putchar('\n');
+		}
+}
