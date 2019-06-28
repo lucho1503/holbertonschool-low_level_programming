bf7e27d76774cc896e7abdb74f123c618d3c774c
lucho1503 <luis.2019.corpha@gmail.com>
2019-06-28 00:58:04 +0000

    fizz_buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,7 +10,7 @@
 
 int main(void)
 {
-int x;
+long int x;
 for (x = 1 ; x <= 100; x++)
 {
 if ((x % 3 || x % 5) == 0)
@@ -27,7 +27,11 @@ printf("Buzz ");
 }
 else
 {
-printf("%d ", x);
+printf("%ld ", x);
+}
+if (x != 100)
+{
+putchar(' ');
 }
 }
 printf("\n");
