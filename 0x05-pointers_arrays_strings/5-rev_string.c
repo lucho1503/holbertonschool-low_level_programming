3b4669d6e329a801dae70c0da8be9d227fce31a1
lucho1503 <luis.2019.corpha@gmail.com>
2019-07-02 21:48:42 +0000

    reverse

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,26 +1,25 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * rev_string - reverse.
+ * @s: containts string
+ * Return: 0 if is succesfull.
  */
 
 void rev_string(char *s)
-{		
-	int l = 0, m = 0;
-	char ls[700];
-
-	while(*(s + m))
-		{
-			ls[m] = s[m];
-			m++;
-		}
-	m = m -1;
-	while (m >= 0)
-		{
-			s[m] = ls[l];
-			l++;
-			m--;
-		}
+{
+int l = 0, m = 0;
+char ls[700];
+while (*(s + m))
+{
+ls[m] = s[m];
+m++;
+}
+m = m - 1;
+while (m >= 0)
+{
+s[m] = ls[l];
+l++;
+m--;
+}
 }
