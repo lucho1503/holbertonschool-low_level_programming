3402808f4631843db91e97532733c01c0976d9d1
lucho1503 <luis.2019.corpha@gmail.com>
2019-07-02 16:25:48 +0000

    pritn_string

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,17 +1,16 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * _puts - print a string characters.
+ * @str: containt the string.
+ * Return: string.
  */
 
 void _puts(char *str)
 {
-	
-	int i = 0;
-	for (; *str != 0; str++)
-	_putchar(str[i]);
-	i++;
-	_putchar('\n');
+int i = 0;
+for (; *str != 0; str++)
+_putchar(str[i]);
+i++;
+_putchar('\n');
 }
