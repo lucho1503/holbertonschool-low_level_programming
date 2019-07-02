42caa476a9ea87cc7fb3255f3ea905f8825d8539
lucho1503 <luis.2019.corpha@gmail.com>
2019-07-02 17:03:21 +0000

    update the value

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,12 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * reset_to_98 - update the value.
+ * @n: value.
+ * Return: update the value.
  */
 
 void reset_to_98(int *n)
 {
-     *n = 98;
+*n = 98;
 }
