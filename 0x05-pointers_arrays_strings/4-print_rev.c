462cb945dfa51ee8b023187edf5bd4033ac8a4fe
lucho1503 <luis.2019.corpha@gmail.com>
2019-07-02 16:20:44 +0000

    rev_string

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,5 +1,6 @@
 #include "holberton.h"
 
+
 /**
  * print_rev - print reverse.
  * @s: containt string.
@@ -8,12 +9,10 @@
 
 void print_rev(char *s)
 {
-int i;
-for (; *s != 0; s++)
-
-i = 0;
-for (; *s >= 0 ; s--)
+int i = 0;
+for (; s[i] != 0;)
+i++;
+for (; i >= 0 ; --i)
 _putchar(s[i]);
-;
 _putchar('\n');
 }
