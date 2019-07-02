c8f6cf11b5e869eed4d551c87e83763e3c5b5488
lucho1503 <luis.2019.corpha@gmail.com>
2019-07-02 23:57:30 +0000

    half_print

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -14,10 +14,9 @@ for (; str[z] != 0;)
 {
 z++;
 }
-y = z % 2 == 0;
-for (y = z / 2; y < z; y++)
-	{
+for (y = z % 2 == 0 ? z / 2 : z / 2 + 1; y < z; y++)
+{
 _putchar(str[y]);
-	}
+}
 _putchar('\n');
 }
