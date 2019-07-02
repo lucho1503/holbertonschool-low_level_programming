da63499da58233b54e1f1c7b52a7e53ee7ad45fd
lucho1503 <luis.2019.corpha@gmail.com>
2019-07-02 22:00:10 +0000

    puts 2 to 2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * puts2 - print 0 to 9 of 2 to 2 .
+ * @str: containts string.
+ * Return: 0 if is succesfull.
+ */
+
+void puts2(char *str)
+{
+int i = 0;
+for (; *str != 0; str = str + 2)
+_putchar(str[i]);
+i = i + 2;
+_putchar('\n');
+}
