09ce2c6d797c19f8ce263024d7070868967ac8d0
lucho1503 <luis.2019.corpha@gmail.com>
2019-07-03 00:49:56 +0000

    array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,19 +2,16 @@
 #include <stdio.h>
 
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * print_array - point entry.
+ * @a: poniter.
+ * @n: array.
+ * Return: 0 if is succesfull.
  */
 
 void print_array(int *a, int n)
 {
-	int z = 0;
-	for (; z <= n - 1; z++)
-		printf("%d%s", a[z], z < n - 1 ? ", " : "");
-	printf("\n");
-	
-	
-	
-	
+int z = 0;
+for (; z <= n - 1; z++)
+printf("%d%s", a[z], z < n - 1 ? ", " : "");
+printf("\n");
 }
