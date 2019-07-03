9fb4c31925c784f706c8d7a331e6b6bc54579da4
lucho1503 <luis.2019.corpha@gmail.com>
2019-07-03 02:15:52 +0000

    copy_string

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,18 +1,19 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * *_strcpy - entry point.
+ * @dest: copy string.
+ * @src: copy to dest.
+ * Return: 0 if is succesfull.
  */
 
 char *_strcpy(char *dest, char *src)
 {
-	int z;
-	for (z = 0; src[z] != 0; z++)
-		{
-			dest[z] = src[z];
-		}
-	dest[z] = 0;
-	return dest;
+int z;
+for (z = 0; src[z] != 0; z++)
+{
+dest[z] = src[z];
+}
+dest[z] = 0;
+return (dest);
 }
