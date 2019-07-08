8700b18490cbc41165be09c4482764a13aabc6ee
lucho1503 <luis.2019.corpha@gmail.com>
2019-07-08 23:19:53 +0000

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,7 +10,17 @@
 char *_strchr(char *s, char c)
 {
 int i;
-for (i = 0; s[i] == c; i++)
-s[i] = c;
-return (s + 2);
+for (i = 0; s[i] != 0; i++)
+	{
+		if (*s == c)
+			{
+				return (s);
+			}
+		else
+			{
+				s++;
+			}
+	}
+		return (s);
+		
 }
