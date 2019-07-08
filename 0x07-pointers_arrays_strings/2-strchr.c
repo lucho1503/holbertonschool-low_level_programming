973e9eb45c965ca78927d815adf4b0561bd8f984
lucho1503 <luis.2019.corpha@gmail.com>
2019-07-08 23:12:30 +0000

    function_strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,15 +1,16 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * *_strchr - entry point.
+ * @s: pointer to c.
+ * @c: string of test.
+ * Return: string to s pointer.
  */
 
 char *_strchr(char *s, char c)
 {
-	int i;
-	for (i = 0; s[i] == c;i++)
-		s[i] = c;
-			return (s + 2);
+int i;
+for (i = 0; s[i] == c; i++)
+s[i] = c;
+return (s + 2);
 }
