81cd4f6b4aeaa06f9f3e0aa90dcaaad50279deeb
lucho1503 <luis.2019.corpha@gmail.com>
2019-07-09 00:54:53 +0000

    print_diag

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -10,10 +10,16 @@
 void print_diagsums(int *a, int size)
 {
 int i, x = 0, y;
+int j, b = 0, c;
 for (i = 0; i <= (size - 1); i++)
 {
 y = (size + 1) * i;
 x = x + *(a + y);
 }
-printf("%d \n", x);
+for (j = 1; j <= size ; j++)
+{
+c = (size - 1) * j;
+b = b + *(a + c);
+}
+printf("%d, %d \n", x, b);
 }
