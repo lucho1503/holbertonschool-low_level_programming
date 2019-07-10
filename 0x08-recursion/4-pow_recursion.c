1d6b7ba3b904e0f70facfcdfb9d2eacdfd31ea91
lucho1503 <luis.2019.corpha@gmail.com>
2019-07-10 22:49:55 +0000

    recursion_pow

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,23 +1,24 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * _pow_recursion - entry point.
+ * @x: integer.
+ * @y: integer.
+ * Return: x pow y.
  */
 
 int _pow_recursion(int x, int y)
 {
-	if (y < 0)
-		{
-			return (-1);
-		}
-	else if (y == 0)
-		{
-			return (1);
-		}
-	else
-		{
-			return (x * _pow_recursion(x, (y - 1)));
-		}
+if (y < 0)
+{
+return (-1);
+}
+else if (y == 0)
+{
+return (1);
+}
+else
+{
+return (x * _pow_recursion(x, (y - 1)));
+}
 }
