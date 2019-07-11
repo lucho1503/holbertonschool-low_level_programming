14d84402d768037eda63197f1894022112b3a211
lucho1503 <lmmc.1503@gmail.com>
2019-07-11 04:51:59 +0000

    square_root

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,50 +1,49 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * _sqrt_recursion - entry point.
+ * @n: integer variable.
+ * Return: root of n.
  */
 
 int _sqrt_recursion(int n)
 {
-	if (n < 0)
-		{
-			return (-1);
-		}
-	else if (n == 0)
-		{
-			return (0);
-		}
-	else if (n == 1)
-		{
-			return (1);
-		}
-	else
-		{
-			return (raiz (n, 1));
-		}
+if (n < 0)
+{
+return (-1);
+}
+else if (n == 0)
+{
+return (0);
+}
+else if (n == 1)
+{
+return (1);
+}
+else
+{
+return (raiz(n, 1));
+}
 }
 
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * raiz - entry point.
+ * @n: integer variable.
+ * @i: integer variable iterative.
+ * Return: root of r.
  */
-
 int raiz(int n, int i)
 {
-	
-	if (n == (i * i))
-		{
-			return (i);
-		}
-	else if (n > (i * i))
-		{
-			return (raiz (n, i + 1));
-		}
-	else
-		{
-			return (-1); 
-		}
+if (n == (i * i))
+{
+return (i);
+}
+else if (n > (i * i))
+{
+return (raiz(n, i + 1));
+}
+else
+{
+return (-1);
+}
 }
