a024cb77ac113f9f414f254380856f39fe24e7c4
lucho1503 <lmmc.1503@gmail.com>
2019-07-14 22:54:42 +0000

    count_args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - entry point.
+ * @argc: count arguments.
+ * @argv: unused attribute.
+ * Return: 0.
+ */
+
+int main(int argc, __attribute__((__unused__)) char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
--- 0x0A-argc_argv/holberton.h
+++ 0x0A-argc_argv/holberton.h
@@ -1,7 +1,8 @@
 #ifndef _SAMPLE_HEADER_H_
 #define _SAMPLE_HEADER_H_
 
-int main (int argc, char * argv []);
+int main(int argc, char *argv[]);
 int _putchar(char c);
+nt main(int argc, __attribute__((__unused__)) char *argv[]);
 
 #endif /* _SAMPLE_HEADER_H_ */
