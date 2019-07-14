6d42a4f65eba2d3545d01baf295b76f92669dbe5
lucho1503 <lmmc.1503@gmail.com>
2019-07-14 22:34:09 +0000

    whatsname

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,15 @@
 #include "holberton.h"
 #include <stdio.h>
 
-int main (int __attribute__((__unused__)) argc, char * argv [])
+/**
+ * main - entry point.
+ * @argv: pointer to matriz.
+ * @argc: unused attribute.
+ *
+ * Return: 0.
+ */
+
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
