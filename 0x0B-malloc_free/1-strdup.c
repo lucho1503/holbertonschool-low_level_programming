33f6abd1efe5ebaabf9a0e69abfbdba1952a55f8
lucho1503 <luis.2019.corpha@gmail.com>
2019-07-16 18:59:19 +0000

    duplicate_string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * *_strdup - entry point.
+ * @str: pointer to string.
+ * Return: string duplicate (dest).
+ */
+
+char *_strdup(char *str)
+{
+char *dest;
+int i;
+dest = malloc(sizeof(str));
+if (str == NULL)
+{
+return (NULL);
+}
+for (i = 0; str[i] != 0; i++)
+{
+dest[i] = str[i];
+}
+if (dest == NULL)
+{
+return (NULL);
+}
+return (dest);
+}
