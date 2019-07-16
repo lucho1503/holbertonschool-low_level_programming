164dd17ca4e84d63a3b113e3329e3e7468953a93
lucho1503 <luis.2019.corpha@gmail.com>
2019-07-16 23:12:13 +0000

    pointer to matriz bidimiensional

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,47 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * **alloc_grid - entry point.
+ * @width: width of array or columns.
+ * @height: height of array or rows.
+ * Return: pointer to array bidimensional.
+ */
+
+int **alloc_grid(int width, int height)
+{
+int i, j;
+int **p;
+if (width <= 0 || height <= 0)
+{
+return (NULL);
+}
+p = malloc(height * sizeof(int));
+if (p == NULL)
+{
+free(p);
+return (NULL);
+}
+for (i = 0; i < height; i++)
+{
+p[i] = malloc(width * sizeof(int));
+if (p[i] == NULL)
+{
+for (; i >= 0; i--)
+{
+free(p[i]);
+}
+free(p);
+return (NULL);
+}
+}
+for (i = 0; i < height; i++)
+{
+for (j = 0; j < width; j++)
+{
+p[i][j] = 0;
+}
+}
+return (p);
+}
