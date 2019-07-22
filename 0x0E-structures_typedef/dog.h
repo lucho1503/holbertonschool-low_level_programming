d34b969a1fe7bc0b42346d92b9151739bf3aed9d
lucho1503 <luis.2019.corpha@gmail.com>
2019-07-22 20:53:58 +0000

    header_file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef STRUCT_DOG
+#define STRUCT_DOG
+
+/**
+ * struct dog - struct the dog that have three variables.
+ * @name: variable that store data types name.
+ * @age: variable thar store data types age.
+ * @owner: variable that store data types owner.
+ * Description: name the struct is dog.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /* STRUCT_DOG */
