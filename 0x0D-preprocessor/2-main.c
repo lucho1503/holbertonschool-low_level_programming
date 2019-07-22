c7e493b194c53e2d95c53cf161d570f6db3f722c
lucho1503 <luis.2019.corpha@gmail.com>
2019-07-22 17:34:48 +0000

    print_file_compile

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
-int main()
+/**
+ * main - entry point.
+ * Return: name of file.
+ **/
+
+int main(void)
 {
-        printf(__FILE__"\n");
-	return (0);
+printf(__FILE__"\n");
+return (0);
 }
