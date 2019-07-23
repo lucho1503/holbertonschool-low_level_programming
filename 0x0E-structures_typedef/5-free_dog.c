4629f630695739fd73665f7747e564e13986553c
lucho1503 <lmmc.1503@gmail.com>
2019-07-23 04:36:03 +0000

    frees_functios

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - template betty.
+ * @d: pointer to struct.
+ * Description: frees all malloc call functios.
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
