5c23a713dbc8f66890ccfdba9792fe09d5ad1eff
lucho1503 <lmmc.1503@gmail.com>
2019-07-23 04:12:28 +0000

    new_dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,43 +10,36 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-struct dog *new_dog;
-int i = 0, j = 0;
-if (name == NULL || owner == NULL)
-{
-return (NULL);
-}
-while (name[i])
-{
-i++;
-}
-while (owner[j])
-{
-j++;
-}
-new_dog = malloc(sizeof(struct dog));
-if (new_dog == NULL)
-{
-return (NULL);
-}
-new_dog->name = malloc((i + 1) * sizeof(char));
-if (new_dog->name == NULL)
-{
-return (NULL);
-}
-new_dog->owner = malloc((j + 1) * sizeof(char));
-if (new_dog->owner == NULL)
-{
-return (NULL);
-}
-for (i = 0; name[i]; i++)
-{
-new_dog->name[i] = name[i];
-}
-for (j = 0; owner[j]; j++)
-{
-new_dog->owner[j] = owner[j];
-}
-new_dog->age = age;
-return (new_dog);
+	dog_t *new_dog = malloc(sizeof(dog_t));
+	int i = 0, j = 0;
+	if (new_dog == NULL)
+	{
+		return (new_dog);
+	}
+	while (name[i])
+	{
+		i++;
+	}
+	while (owner[j])
+	{
+		j++;
+	}
+	new_dog->name = malloc((i + 1) * sizeof(char));
+	new_dog->owner = malloc((j + 1) * sizeof(char));
+	if (new_dog->owner == NULL || new_dog->name == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; name[i]; i++)
+	{
+		new_dog->name[i] = name[i];
+	}
+	for (j = 0; owner[j]; j++)
+	{
+		new_dog->owner[j] = owner[j];
+	}
+	new_dog->name[i] = 0;
+	new_dog->owner[j] = 0;
+	new_dog->age = age;
+	return (new_dog);
 }
