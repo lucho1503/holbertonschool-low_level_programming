323c1d70fb5f1084558316be0b95312dcd3abb06
lucho1503 <luis.2019.corpha@gmail.com>
2019-07-23 00:18:41 +0000

    new_struct

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,53 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * new_dog - betty style entry point.
+ * @name: name dog in struct.
+ * @age: age dog in struct.
+ * @owner: owner dog in struct.
+ * Return: pointer to struct new.
+ * Description: create a new struct dog.
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+struct dog *l = NULL;
+int k, i = 0, j = 0;
+if (name == NULL || owner == NULL)
+{
+return (NULL);
+}
+while (name[i] != 0)
+{
+i++;
+}
+while (owner[j] != 0)
+{
+j++;
+}
+l = malloc(sizeof(struct dog));
+if (l == NULL)
+{
+free(l);
+return (NULL);
+}
+(*l).name = malloc(i + 1);
+if ((*l).name == NULL)
+{
+free((*l).name);
+}
+(*l).owner = malloc(j + 1);
+if ((*l).owner == NULL)
+{
+free((*l).owner);
+}
+for (k = 0; k < i; k++)
+{
+(*l).name[k] = name[k];
+}
+for (k = 0; k < j; k++)
+{
+(*l).owner[k] = owner[k];
+}
+(*l).age = age;
+return (l);
+}
