efb1854eb6e43a3e95eb9bf377cae330922e37e0
lucho1503 <luis.2019.corpha@gmail.com>
2019-07-27 19:01:04 +0000

    header file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,20 +1,18 @@
 #ifndef VARIADIC
 #define VARIADIC
 #include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+int _putchar(char c);
 
 typedef struct pt {
-	char *pt;
-	char (*p)(va_list *);
-} pt_r;
-
-void print_c(va_list *);
-void print_i(va_list *);
-void print_f(va_list *);
-void print_s(va_list *);
+	char t;
+	void (*p)(va_list);
+} mr;
 
 #endif /* VARIADIC */
