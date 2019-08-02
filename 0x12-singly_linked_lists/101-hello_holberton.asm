6c420ec1b3cc9c204e1becfbd06355eb7732e282
lucho1503 <lmmc.1503@gmail.com>
2019-08-02 04:44:24 +0000

    101-hello_holberton.asm

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,17 @@
+section .text
+	global main
+
+main:
+	mov rax, 1		; write(
+	mov rdi, 1		;	STDOUT_FILENO,
+	mov rsi, msg		;	"hello, world\n",
+	mov rdx, msglen		;	sizeof("Hello, world\n")
+	syscall			;	);
+
+	mov rax, 60		; exit(
+	mov rdi, 0		;	EXIT_SUCCES
+	syscall			; );
+
+	section .rodata
+msg:		db "Hello, Holberton", 10
+msglen:		equ $ -msg
