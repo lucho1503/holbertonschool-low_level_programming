44fb699f4bc1a08b150c58167c98aa80fa2a3671
lucho1503 <lmmc.1503@gmail.com>
2019-08-02 05:04:07 +0000

    first_function print

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * __start - function print before main function.
+ *
+ * Return: nothing.
+ */
+void __attribute__((constructor)) __start()
+{
+	printf("You're beat! and yet, you must allow,\n");
+		printf("I bore my house upon my back!\n");
+}
