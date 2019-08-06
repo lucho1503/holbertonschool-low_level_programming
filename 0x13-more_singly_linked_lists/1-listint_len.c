34b5fd7acc77bdfc10f12f89e6061e44f3b8e96f
lucho1503 <lmmc.1503@gmail.com>
2019-08-06 15:10:36 +0000

    list_len

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -10,12 +10,12 @@ size_t listint_len(const listint_t *h)
 {
 	int i;
 
-	if (h == NULL)
-	{
-		return (0);
-	}
 	while (h)
 	{
+		if (h == NULL)
+		{
+			return (0);
+		}
 		h = h->next;
 		i++;
 	}
