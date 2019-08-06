b224529dc0153437e632c9619f45b1f941594820
lucho1503 <lmmc.1503@gmail.com>
2019-08-06 17:40:14 +0000

    free list2

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,14 +10,14 @@ void free_listint2(listint_t **head)
 {
 	listint_t *f;
 
+	if (head == NULL)
+	{
+		return;
+	}
 	while (*head)
 	{
 		f = *head;
 		*head = f->next;
 		free(f);
-	}
-	if (head == NULL)
-	{
-		return ;
 	}
 }
