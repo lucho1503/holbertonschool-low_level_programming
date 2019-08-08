8ef04a2c63799343630f7f4fed3786a23af9581e
lucho1503 <lmmc.1503@gmail.com>
2019-08-08 01:36:31 +0000

    insert_new node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -32,7 +32,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		}
 		node->next = ptr->next;
 		ptr->next = node;
+		node->n = n;
 	}
-	node->n = n;
+	
 	return (*head);
 }
