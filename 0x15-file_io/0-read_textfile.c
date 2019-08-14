f34d5984d71007d8eeb2bd6a9036440a7eb71b29
lucho1503 <lmmc.1503@gmail.com>
2019-08-14 03:53:51 +0000

    read_file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * read_textfile - entry point.
+ * @filename: name of file.
+ * @letters: number of letters to print.
+ * Return: count of characters.
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int file;
+	int i = 0;
+	char buffer[102400];
+
+	if (filename == NULL)
+	{
+		return (0);
+	}
+	file = open(filename, O_RDONLY);	
+	if (file == -1)
+	{
+		return (0);
+	}
+	i = read (file, buffer, letters);
+	buffer[i] = 0;
+	write (STDOUT_FILENO, buffer, i);
+	close(file);
+	return (i);
+	
+}
