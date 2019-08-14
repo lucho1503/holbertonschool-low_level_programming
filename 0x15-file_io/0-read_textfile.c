a0a6189b7da39cfaae99b736a45911bf4fc8d8df
lucho1503 <lmmc.1503@gmail.com>
2019-08-14 03:57:40 +0000

    read_text file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,21 +11,20 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int file;
 	int i = 0;
-	char buffer[102400];
+	char buffer[1024];
 
 	if (filename == NULL)
 	{
 		return (0);
 	}
-	file = open(filename, O_RDONLY);	
+	file = open(filename, O_RDONLY);
 	if (file == -1)
 	{
 		return (0);
 	}
-	i = read (file, buffer, letters);
+	i = read(file, buffer, letters);
 	buffer[i] = 0;
-	write (STDOUT_FILENO, buffer, i);
+	write(STDOUT_FILENO, buffer, i);
 	close(file);
 	return (i);
-	
 }
