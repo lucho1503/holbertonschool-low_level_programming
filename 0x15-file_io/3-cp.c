5023b5e351d1e69baae6b99bdf182fb173df52ec
lucho1503 <lmmc.1503@gmail.com>
2019-08-15 20:06:32 +0000

    copy_file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -14,37 +14,37 @@ int main(int ac, char **av)
 
 	if (ac != 3)
 	{
-		exit(97);
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"); }
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97); }
 	fd1 = open(av[1], O_RDONLY);
 	if (fd1 == -1)
 	{
-		exit(98);
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[1]); }
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[1]);
+		exit(98); }
 	fd2 = open(av[2], O_RDWR | O_CREAT | O_TRUNC, 0664);
 	if (fd2 == -1)
 	{
-		exit(99);
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]); }
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		exit(99); }
 	while ((i = read(fd1, buffer, 1024)) != 0)
 	{
 		if (i == -1)
 		{
-			exit(98);
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[1]); }
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[1]);
+			exit(98); }
 		w = write(fd2, buffer, i);
 		if (w == -1)
 		{
-			exit(99);
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]); }}
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+			exit(99); }}
 	cl1 = close(fd1), cl2 = close(fd2);
 	if (cl1 == -1)
 	{
-		exit(100);
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1); }
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1);
+		exit(100); }
 	if (cl2 == -1)
 	{
-		exit(100);
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2); }
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2);
+		exit(100); }
 	return (0);
 }
