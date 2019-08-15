9536d471075150b057eafd0e8942de612bf70b43
lucho1503 <lmmc.1503@gmail.com>
2019-08-15 17:00:31 +0000

    create_file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+ * create_file - enry point.
+ * @filename: name of file.
+ * @text_content: content of file.
+ * Return: 1 if is succesfull else -1.
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, w;
+	int i;
+
+	for (i = 0; text_content[i]; i++)
+	{
+	}
+	if (text_content == NULL)
+	{
+		return (1);
+	}
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	w = write(fd, text_content, i);
+	if (fd == -1 && w == 0 && filename == NULL)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (1);
+	}
+	close(fd);
+	return (1);
+}
