521c459e9f3ab290a7503ecaea382521eb6fddb6
lucho1503 <lmmc.1503@gmail.com>
2019-08-15 17:11:30 +0000

    create_file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,28 +9,25 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd, w;
+	int fd;
 	int i;
 
 	if (filename == NULL)
 	{
 		return (-1);
 	}
-	
-	{
-	}
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (text_content == NULL)
 	{
 		return (1);
 	}
-	for (i = 0; text_content[i]; i++)
-	{}
-	w = write(fd, text_content, i);
-	if (fd == -1 && w == 0)
+	if (fd == -1)
 	{
 		return (-1);
 	}
+	for (i = 0; text_content[i]; i++)
+	{}
+	write(fd, text_content, i);
 	close(fd);
 	return (1);
 }
