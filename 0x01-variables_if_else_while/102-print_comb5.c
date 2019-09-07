98d4af4c2b666c0aa5bc566ba679aad45794df5a
lucho1503 <lmmc.1503@gmail.com>
2019-09-07 21:02:41 +0000

    comb_four_numbers_print

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main - entry point.
+ *
+ * Return: 0 if is success.
+ */
+
+int main(void)
+{
+	int i, j, k, l;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '0'; j <= '9'; j++)
+		{
+			for (k = '0'; k <= '9'; k++)
+			{
+				for (l = '1'; l <= '9'; l++)
+				{
+					if (((i * 10) + j) < ((k * 10) + l))
+					{
+						putchar(i);
+						putchar(j);
+						putchar(' ');
+						putchar(k);
+						putchar(l);
+					if (i != '9' || j != '8' || k != '9' || l != '9')
+					{
+						putchar(',');
+						putchar(' ');
+					}
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
