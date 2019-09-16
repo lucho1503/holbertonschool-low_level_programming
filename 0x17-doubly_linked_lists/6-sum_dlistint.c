adc990b4f5a44bc02c6e97ee1d58a3cee062be7e
lucho1503 <lmmc.1503@gmail.com>
2019-09-16 02:40:16 +0000

    sum_all

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - sum all the lists.
+ * @head: pointer to first element
+ * Return: sum the alls lists.
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	int suma;
+
+	suma = head->n;
+	while (head->next)
+	{
+		if (head)
+		{
+			head = head->next;
+			suma += head->n;
+		}
+		else
+			return (0);
+	}
+	return (suma);
+}
