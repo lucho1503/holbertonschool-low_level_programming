c559669c721ff17e85b5dfb673cc1e0f4d0216f8
lucho1503 <lmmc.1503@gmail.com>
2019-10-11 22:43:33 +0000

    1-djb2.c

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,4 +1,5 @@
 #include "hash_tables.h"
+#define TABLE 1000000000000
 
 /**
  * hash_djb2 - implemented has function.
@@ -11,10 +12,10 @@ unsigned long int hash_djb2(const unsigned char *str)
 	unsigned long int hash;
 	int c;
 
-	hash = 5831;
+	hash = 5381;
 	while ((c = *str++))
 	{
-		hash = hash * 33 + c;
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
 	}
 	return (hash);
 }
