8bc725b7341715fbc2f5f82d127f74bc2feb6a1a
lucho1503 <lmmc.1503@gmail.com>
2019-10-12 00:27:23 +0000

    fix

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -10,15 +10,8 @@
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	unsigned long int hash;
-	unsigned long int i;
+	unsigned long int i = 0;
 
-	hash = 0;
-	i = 0;
-	while (key && key[i])
-	{
-		hash = (hash + key[i]) % size;
-		i++;
-	}
-	return (hash);
+	i = hash_djb2(key) % size;
+	return (i);
 }
