d4370c4d8ad5f5ef9420f7ae7d44c4ec0f50c8b7
lucho1503 <lmmc.1503@gmail.com>
2019-10-13 04:34:29 +0000

    fix

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -40,7 +40,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	node->key = strdup(key);
 	node->value = strdup(value);
 	node->next = NULL;
-	if (ht->array[index] == NULL)
+	if (ht->array[index])
 	{
 		node->next = ht->array[index];
 	}
