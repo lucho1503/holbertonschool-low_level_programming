f481b750a39a73fadd26bc636d74e5a228e20633
lucho1503 <lmmc.1503@gmail.com>
2019-10-13 04:20:30 +0000

    3-hash_table_set.c

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,49 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - add a element to hash table.
+ * @ht: hash_table element
+ * @key: key of element.
+ * @value: value of the key.
+ * Return: 1 if is succesful or 0 if not.
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index = 0;
+	hash_node_t *node;
+	hash_node_t *arr;
+
+	if (ht == NULL)
+		return (0);
+	if (strcmp(key, "") == 0)
+	{
+		return (0);
+	}
+	index = key_index((const unsigned char *) key, ht->size);
+	arr = ht->array[index];
+	while (arr)
+	{
+		if (arr->key == key)
+		{
+			free(arr->value);
+			arr->value = strdup(value);
+			return (1);
+		}
+		arr = arr->next;
+	}
+	node = malloc(sizeof(hash_node_t *));
+	if (node == NULL)
+	{
+		return (0);
+	}
+	node->key = strdup(key);
+	node->value = strdup(value);
+	node->next = NULL;
+	if (ht->array[index] == NULL)
+	{
+		node->next = ht->array[index];
+	}
+	ht->array[index] = node;
+	return (1);
+}
