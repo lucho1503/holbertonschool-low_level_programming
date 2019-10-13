005bd32cbd6736c531d1c70155e60d1ea20edc53
lucho1503 <lmmc.1503@gmail.com>
2019-10-13 04:32:52 +0000

    fixed_

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,10 +11,10 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index = 0;
-	hash_node_t *node;
-	hash_node_t *arr;
+	hash_node_t *node = NULL;
+	hash_node_t *arr = NULL;
 
-	if (ht == NULL || key == NULL || value == NULL)
+	if (ht == NULL || key == NULL || value == NULL || ht->array == NULL)
 		return (0);
 	if (!strcmp(key, ""))
 	{
