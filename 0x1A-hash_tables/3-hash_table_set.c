9dfa4ab6f1ac595594306a731968b9a05ac25473
lucho1503 <lmmc.1503@gmail.com>
2019-10-13 04:38:30 +0000

    fix-3-hash

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -32,7 +32,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		}
 		arr = arr->next;
 	}
-	node = malloc(sizeof(hash_node_t *));
+	node = calloc(1, sizeof(hash_node_t *));
 	if (node == NULL)
 	{
 		return (0);
