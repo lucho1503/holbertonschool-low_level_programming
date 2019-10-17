7e932e6ef2f04e9b71266744919572e921df9410
lucho1503 <lmmc.1503@gmail.com>
2019-10-17 03:22:45 +0000

    0-fixed

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,14 +8,15 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *new_table;
+	hash_table_t *new_table = NULL;
+	unsigned long int i;
 
 	new_table = malloc(sizeof(hash_table_t));
 	if (new_table == NULL)
 	{
 		return (NULL);
 	}
-	new_table->array = malloc(sizeof(hash_node_t *));
+	new_table->array = malloc(sizeof(hash_node_t *) * size);
 	if (new_table->array == NULL)
 	{
 		free(new_table);
@@ -23,5 +24,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	}
 	
 	new_table->size = size;
+	for (i = 0; i < size; i++)
+		new_table->array[i] = NULL;
 	return (new_table);
 }
