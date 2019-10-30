a85e42fb684e5e44abb54894eacc50c8b5903895
lucho1503 <lmmc.1503@gmail.com>
2019-10-30 03:09:00 +0000

    0-bubble_sort.c

---
+++ 0x1B-sorting_algorithms/0-bubble_sort.c
@@ -0,0 +1,27 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sort a array.
+ * @size: size of array
+ * @array: array of size (size)
+ * Return: Nothing
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j, temp;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+			}
+		}
+	}
+}
