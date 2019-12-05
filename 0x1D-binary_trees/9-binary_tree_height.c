09b9ba05684e0387b98b0a6e51c5fa2637ff9e4a
lucho1503 <lmmc.1503@gmail.com>
2019-12-05 02:23:34 +0000

    9-binary_tree_height.c

--- 0x1D-binary_trees/9-binary_tree_height.c
+++ 0x1D-binary_trees/9-binary_tree_height.c
@@ -10,7 +10,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
-	if (!tree->left && !tree->right)
+	if (tree->left == NULL && tree->right == NULL)
 		return (0);
 	else
 		return (1 + (binary_tree_height(tree->left),
