f6a8bb3b1176a9c3abfc1fe7964ba0820647db48
lucho1503 <lmmc.1503@gmail.com>
2019-12-06 17:49:21 +0000

    add conditional

--- 0x1D-binary_trees/16-binary_tree_is_perfect.c
+++ 0x1D-binary_trees/16-binary_tree_is_perfect.c
@@ -40,6 +40,8 @@ size_t binary_tree_size(const binary_tree_t *tree)
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
+	if (tree == NULL)
+		return (0);
 	if (binary_tree_size(tree->left) == binary_tree_size(tree->right))
 		return (1);
 	if (binary_tree_size(tree->left) < binary_tree_size(tree->right))
