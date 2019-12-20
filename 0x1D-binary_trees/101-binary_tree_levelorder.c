04f8dee0510d1f9bca43114701098e3029e99225
lucho1503 <lmmc.1503@gmail.com>
2019-12-20 20:53:18 +0000

    101-binary_tree_levelorder.c

--- 0x1D-binary_trees/101-binary_tree_levelorder.c
+++ 0x1D-binary_trees/101-binary_tree_levelorder.c
@@ -1,6 +1,54 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: pointer to the root node of the tree.
+ * Return: height of the tree or 0 if fail.
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t l = 0, r = 0;
+
+	if (tree != NULL)
+	{
+		if (tree->left)
+			l = 1 + binary_tree_height(tree->left);
+		if (tree->right)
+			r = 1 + binary_tree_height(tree->right);
+
+		if (l > r)
+			return (l);
+		else
+			return (r);
+	}
+	else
+		return (0);
+}
+
+/**
+ * level_print - print the nodes in the same level
+ * @tree: pointer to the root node of the tree
+ * @func: pointer to a function to call for each node
+ * @count: level of pointer
+ * Return: Nothing.
+ */
+
+void level_print(const binary_tree_t *tree, void (*func)(int), int count)
+{
+	if (tree != NULL && func != NULL)
+	{
+		if (count == 0)
+			func(tree->n);
+		else if (count > 0)
+		{
+			level_print(tree->left, *func, count - 1);
+			level_print(tree->right, *func, count - 1);
+		}
+	}
+}
+
+/**
  * binary_tree_levelorder - goes through a binary tree using level-order
  * @tree: pointer to the root node of the tree
  * @func: pointer to a function to call for each node
@@ -9,16 +57,15 @@
 
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree->parent == NULL)
-		func(tree->n);
-	if (tree->left != NULL)
+	int level = 0, count = 0;
+
+	if (tree == NULL || func == NULL)
+		return;
+
+	level = binary_tree_height(tree);
+
+	for (count = 0; count <= level; count++)
 	{
-		func(tree->left->n);
-		binary_tree_levelorder(tree->left, func);
-	}
-	if (tree->right != NULL)
-	{
-		func(tree->right->n);
-		binary_tree_levelorder(tree->right, func);
+		level_print(tree, *func, count);
 	}
 }
