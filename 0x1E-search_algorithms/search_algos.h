a34f642503973f48388187bd5c126d280dd9802a
lucho1503 <lmmc.1503@gmail.com>
2020-01-16 16:42:04 +0000

    search_algos.h

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,12 @@
+#ifndef S_A
+#define S_A
+
+
+#include "search_algos.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif /* S_A */
